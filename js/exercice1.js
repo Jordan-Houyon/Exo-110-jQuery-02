98f01162f8023de06a73c80ea0c369afd6d6c4f5
arthu <jordnhouyon@outlook.fr>
2023-01-03 09:19:20 +0100

    exercice finished

---
+++ js/exercice1.js
@@ -0,0 +1,9 @@
+$('li').text("All about unicorns");
+
+// update title
+
+$("page-heading").text("All about unicorns");
+
+// updtae  alls balises  class "animal" to "unicorns"
+
+$("span").filter('.animal').text("unicorns");
